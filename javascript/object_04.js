a95b76974f910fe8d86226a383efbf9c221ead02
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-03-31 12:49:44 +0000

    object_04 Complete

---
+++ javascript/object_04.js
@@ -0,0 +1,13 @@
+const course = {
+    course_name: "JavaScript",
+    price: 999,
+    courseInstructor: "Ashish"
+};
+
+//console.log(course.courseInstructor); // This will output: "Ashish"
+//const{courseInstructor:Instructor}=course
+//console.log(Instructor);
+const navbar={
+    
+}
+ 
