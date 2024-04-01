27b7012f1caf0e28614764fd321f7d06c34e4c7d
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-04-01 12:48:14 +0000

    loop part)1

---
+++ basic_loop/control_flow.js
@@ -0,0 +1,59 @@
+// const temperture=50;
+
+
+
+// if(temperture<50)
+// {
+//      console.log("less than 50 temperture");
+// }
+// else{
+//     console.log("more than 50 temperture");
+// }
+
+
+// if(temperture==="50")
+// {
+//      console.log("less than 50 temperture");
+// }
+// else{
+//     console.log("more than 50 temperture");
+// }
+// console.log("Execute the instruction");
+
+//  const score=200
+// if(score>100){
+//   const power="fly"
+//     console.log(`User power:${power}`);
+// }
+// // console.log(`User power:${power}`);
+// const balance=1000
+// if(balance>500) console.log("test"),console.log("test2");
+// const  balance=1000
+// if(balance<500){
+//     console.log("less than 500");
+// }
+// else if(balance<750){
+//     console.log("less than 750");
+// }
+// else if(balance<900){
+//     console.log("less than 900");
+// }
+// else {
+//     console.log("less  than 1200");
+// }
+const UserloggedIn=true
+const debit=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+const GuestUser=true
+
+ if(UserloggedIn && debit  && 2==3)
+{
+console.log("Allow the access");
+}  
+  if(UserloggedIn ||loggedInFromEmail|| GuestUser)
+   {
+      console.log("User logged In");
+  }
+
+
