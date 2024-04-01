10680f4b502528b05f98912cf5c6486f97e1545a
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-04-01 12:48:43 +0000

    loop_part02

---
+++ basic_loop/switch.js
@@ -0,0 +1,37 @@
+// const  month=3
+
+// switch(month){
+
+// case 1:
+// console.log("January");
+//  break;
+//  case 2:
+//     console.log("February");
+//     break;
+//     case 3:
+//         console.log("March");
+//         break;
+//         case 4:
+//             console.log("April");
+//             break;
+//         default:
+//             console.log("Remaining Month");
+
+// }
+const traffic_color="green";
+switch (traffic_color) {
+    case "red":
+        console.log(" To Stop the Traffic");
+        
+       break;
+        case "yellow":
+            console.log(" to wait or go slow.");
+            break;
+            case "green":
+                console.log("to drive ahead");
+                 break;
+
+    default:
+         console.log("help pedestrians cross the street safely at intersection");
+        break;
+}
---
+++ basic_loop/turthy_falsy.js
@@ -0,0 +1,43 @@
+// const userEmail="kandpalashish58@gmaill.com"
+// if(userEmail){
+//     console.log("Got user email");
+
+// }
+// else{
+//     console.log("Don't have  user email");
+// }
+// const userEmail=""
+// if(userEmail){
+//     console.log("Got user email");
+
+// }
+// else{
+//     console.log("Don't have  user email");
+// }// According the falsy_js 
+//  const userEmail=[]
+// if(userEmail){
+//     console.log("Got user email");
+
+// }
+// else{
+//     console.log("Don't have  user email");
+// } //  According the statement Ture the Ans give
+// if(userEmail.length===0)
+// {
+//     console.log("Array is Empty");
+// }
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0)
+// {
+//     console.log("Object is empty");
+// }
+//Nullish Coaliscing
+ let val1;
+val1=5?? 10//output:5
+// val1=undefined ?? 15//output:15
+// val1=null??10??20//output:10
+ console.log(val1);
+//  tenary Operator
+
+const iceTeaPrice=100
+iceTeaPrice>=80?console.log("less than 80"):console.log("more than 80");
