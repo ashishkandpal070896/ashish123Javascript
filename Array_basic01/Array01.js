1c0000967788ec0d8ffe93abc0ac3702924b3667
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-04-02 12:46:30 +0000

    commit

--- Array_basic01/Array01.js
+++ Array_basic01/Array01.js
@@ -1,5 +1,27 @@
 //Array 
 const myArr=[0,1,2,3,4,5]
 // console.log(myArr[5]);
-myArr.push(6)
-console.log(myArr);
+//  myArr.push(6,7)//[0,1,2,3,4,5,6,7]
+// myArr.pop()
+// console.log(myArr);
+// myArr.unshift(0)//[0,0,1,2,3,4,5,6,7]
+// myArr.shift()//[1,2,3,4,5,6,7]
+// console.log(myArr);
+// console.log(myArr.includes(9));//boolean :false
+// console.log(myArr.indexOf(23));
+// console.log( typeof myArr);//object
+// const newArr=myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);//object to convert String"
+console.log("A",myArr);
+const myn1=myArr.slice(1,3);
+console.log(myn1);
+console.log("B",myArr);
+const myn2=myArr.splice(1,3);
+console.log("C",myArr);
+console.log(myn2);
+
+
+
+
+
