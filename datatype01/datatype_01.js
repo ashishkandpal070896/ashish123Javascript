fe33a7766bd5efd3099980fc396db84e0a6b9049
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-04-03 09:53:25 +0000

    data_type01

---
+++ datatype01/datatype_01.js
@@ -0,0 +1,4 @@
+// const foo = 42; // foo is a number
+// const result = foo + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+// console.log(result); // 421
+// alert(3+3)
