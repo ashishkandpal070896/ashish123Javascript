40e14ef346585988847081588c320bdbd816a2b4
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-04-04 04:57:33 +0000

    commit

---
+++ datatype01/conversion_02.js
@@ -0,0 +1,31 @@
+"use strict";
+//
+//"""""""""""""""""""""""""""""Operations"""""""""""""""""""""""""""
+// let value =3
+// let negvalue=-value
+// console.log(negvalue);
+// let str1="Hello";
+// let str2=" Ashish";
+// let str3=str1 + str2;
+// console.log(str3);
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log((3 +4) * 5 % 3);
+console.log(+true);
+console.log(+"");//0
+let x = 3;
+const y = x++;//postfix
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;//perfix
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+let countgamer=100;
+++countgamer;
+console.log(countgamer);
