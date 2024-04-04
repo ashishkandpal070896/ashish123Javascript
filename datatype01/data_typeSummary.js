6c4f6f7a6d5cec837df4261ab1ce7ff9807ee3c0
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-04-04 06:19:24 +0000

    commit

---
+++ datatype01/data_typeSummary.js
@@ -0,0 +1,29 @@
+//  There are 7 type Primitive data type 
+//(i)String,(ii)Number,(iii)Boolean,(iv)Null,(v)undefined(vi)Symbol(vii)BigInt
+//Primitive data type is call value hote hain
+//Reference (Non Primitive data type)
+// Array,Function,Object
+//javaScript is a Dynamic types
+// const score=100;
+// const socre1=100.3;
+// console.log(typeof(score));
+// console.log(typeof(socre1));
+// const id=Symbol('123');
+// const anotherId=Symbol('123');
+// console.log(id==anotherId);
+
+// Type of val :Result
+// undefined : "Undefined"
+// Null:  "Object"
+// Number : "Number"
+//Boolean:"Boolean"
+//String:String:         
+//Object (native and:      : "Object"
+// does not :
+//implement [[Call]]):
+//Object (native or host:      "function"
+// and does implement [[Call]])
+// Object (host and 
+// does not  Implement[[Call]])                 
+//                                     : Implementation-defined except                                
+            //                     may not be "undefined", "boolean", "number", or "string".implement [[Call]])
