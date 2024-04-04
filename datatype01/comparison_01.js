80dd0b233aad657609e08e313005992a412795ab
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-04-04 05:22:27 +0000

    commit

---
+++ datatype01/comparison_01.js
@@ -0,0 +1,14 @@
+// console.log(2==2);
+// console.log(2==3);
+// console.log(2!=3);
+// console.log("2"==2);
+// console.log("02"==2);
+
+// console.log("2">1);
+// console.log("02">=2);
+// console.log(null>0);//(null is not greater than 0)
+// console.log(null==0);//(null is not equal to 0) and null is treated as 0
+// console.log(null>=0);//null is considered greater than or equal to 0) and  null is treated as 0
+// console.log(undefined == 0);  // false (undefined is not equal to 0)
+// console.log(undefined >= 0);  // false (undefined is not greater than or equal to 0)
+// console.log(undefined <= 0);  // false (undefined is not less than or equal to 0)
