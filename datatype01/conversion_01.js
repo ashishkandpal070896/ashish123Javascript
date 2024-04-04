a8899d52876486227a855c1901f3b2ffd0d202c1
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-04-04 04:23:21 +0000

    converstion

---
+++ datatype01/conversion_01.js
@@ -0,0 +1,46 @@
+// let score="33abd"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valuenumber=Number(score);
+// console.log( typeof valuenumber);
+// console.log(valuenumber);
+
+// let score=null
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valuenumber=Number(score);
+// console.log( typeof valuenumber);
+// console.log(valuenumber);
+
+// let score=undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valuenumber=Number(score);
+// console.log( typeof valuenumber);
+// console.log(valuenumber);
+
+// let score=true;
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valuenumber=Number(score);
+// console.log( typeof valuenumber);
+// console.log(valuenumber);
+
+// 33 number ko number convert krte hain to number convert ho jata result =33
+//"33abs" is not a number(NAN)
+//ture or false covert ho jata boolean 
+// true value =1 , false value=0
+// boolean me yaddi hum "" rkhte hain to result false
+
+// let count=1;
+// let booleanCount=Boolean(count);
+// console.log(booleanCount);
+
+// let count="";
+// let booleanCount=Boolean(count);
+// console.log(booleanCount);
+
+let Somenumber="33"
+let StringNumber=String(Somenumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
--- datatype01/datatype_01.js
+++ datatype01/datatype_01.js
@@ -1,4 +1,21 @@
-// const foo = 42; // foo is a number
-// const result = foo + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
-// console.log(result); // 421
-// alert(3+3)
+"use strict"//treat all js code as newer version
+const foo = 42; // foo is a number
+ const result = foo + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+console.log(result); // 421
+// alert(3+3)//we are  using node js and not broswering
+
+console.log("Ashih");//code readability should be high
+//data type in Js 
+//NUmber 
+//Bigint
+//String String is uding "" or ''
+//boolean True,false
+//null is standalone value
+//undefined value is not defined
+//Symbol is using uniqueness
+//object 
+//typeof is using datatype respresentation
+//console.log(typeof 2);//number
+console.log(typeof "Ashish");//string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
