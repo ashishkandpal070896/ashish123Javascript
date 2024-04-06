04e088deeba69652ab24111ec3b52a7934588ced
ashishkandpal070896 <147151225+ashishkandpal070896@users.noreply.github.com>
2024-04-06 01:08:25 +0000

    commit

---
+++ datatype01/nums_and_maths.js
@@ -0,0 +1,24 @@
+// const score=400;
+// // console.log(score);
+//  const balance=new Number (100);
+// // console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+// const othernumber=23.8966;
+// console.log(othernumber.toPrecision(3));
+// const hundreds=1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+//**********************************************Maths**************************************************************
+// console.log(Math);
+// console.log(Math.abs(-4));//abs mean absolute value
+// console.log(Math.round(2.36));
+// console.log(Math.ceil(2.34));
+// console.log(Math.floor(2.39));
+// console.log(Math.pow(2,3));
+// console.log(Math.min(2,3,4,5,6));
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max -min+1))+min);
